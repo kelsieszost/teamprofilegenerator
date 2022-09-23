cec6cb092c2970e75063d4572f4b3f99e6442942
kelsieszost <owner@kelsieszost.design>
2022-09-23 16:26:41 -0400

    managerInfo function

--- index.js
+++ index.js
@@ -4,3 +4,71 @@ const employee = require('./lib/employee');
 const manager = require('./lib/manager');
 const engineer = require('./lib/engineer');
 const intern = require('./lib/intern');
+
+var employees = [];
+
+const managerInfo = function () {
+    // prompts for manager
+   return inquirer.prompt([
+        {
+            type: 'text',
+            name: 'name',
+            message: "What is the manager's name?",
+            validate: name => {
+                if (name) {
+                    return true;
+                } else {
+                    console.log('Please enter a name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'id',
+            message: 'What is the employee ID?',
+            validate: id => {
+                if (id) {
+                    return true;
+                } else {
+                    console.log('Please enter an ID');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: 'Enter email address',
+            validate: email => {
+                if (email) {
+                    return true;
+                } else {
+                    console.log('Please enter an email address');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'office',
+            message: 'Enter office number',
+            validate: office => {
+                if (office) {
+                    return true;
+                } else {
+                    console.log('Please enter a office number');
+                    return false;
+                }
+            }
+        },
+    ])
+
+    .then(managerData => {
+        const { name, id, email, officeNumber } = managerData;
+        const manager = new Manager(name, id, email, officeNumber);
+        employees.push(manager);
+       
+      
+    })
+};
